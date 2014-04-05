465fa1ec907882e4c027968b102f652bea2bd214
Matthew Jones <matt@geekle.id.au>
2014-04-05 17:42:02 +1100

    Add servers to the backend that are already configured.

--- start-etcdctl.sh
+++ start-etcdctl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ETCD_PEER="172.17.42.1:4001"
-HAPROXY_BACKEND_SERVICE=$(etcdctl --peers ${ETCD_PEER} get /config/HAPROXY_BACKEND_SERVICE | grep -v "Error: 100: Key not found" || echo "web")
+HAPROXY_BACKEND_SERVICE=$(etcdctl --peers ${ETCD_PEER} get /config/HAPROXY_BACKEND_SERVICE | egrep -v "Error: 100: Key not found|Cannot sync with the cluster" || echo "web")
+for i in $(etcdctl --peers ${ETCD_PEER} ls --recursive /services/web | egrep -v "Error: 100: Key not found|Cannot sync with the cluster"); do
+        ETCD_WATCH_ACTION="set" ETCD_WATCH_KEY=$i ETCD_WATCH_VALUE=$(etcdctl --peers ${ETCD_PEER} get ${ETCD_WATCH_KEY} | egrep -v "Error: 100: Key not found|Cannot sync with the cluster") /update-haproxy.sh
 exec /usr/local/bin/etcdctl --peers ${ETCD_PEER}  exec-watch --recursive /services/$HAPROXY_BACKEND_SERVICE -- /update-haproxy.sh
 
