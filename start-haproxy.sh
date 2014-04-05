6e0f69a344a4d3eb1cbc07153aaab948380c4f6e
Matthew Jones <matt@geekle.id.au>
2014-04-05 17:42:29 +1100

    Ensure that the previous process is finished.

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec /usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg
+exec /usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
