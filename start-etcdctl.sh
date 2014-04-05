eb25af19664494ed67e7617d4d3f4fbea4df7271
Matthew Jones <matt@geekle.id.au>
2014-04-05 16:18:48 +1100

    Add peer host and fix if key doesn't exist

--- start-etcdctl.sh
+++ start-etcdctl.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-HAPROXY_BACKEND_SERVICE=$(etcdctl get /config/HAPROXY_BACKEND_SERVICE || "web")
-exec /usr/local/bin/etcdctl --peers '172.17.42.1:4001'  exec-watch --recursive /services/$HAPROXY_BACKEND_SERVICE -- update-haproxy.sh
+ETCD_PEER="172.17.42.1:4001"
+HAPROXY_BACKEND_SERVICE=$(etcdctl --peers ${ETCD_PEER} get /config/HAPROXY_BACKEND_SERVICE | grep -v "Error: 100: Key not found" || echo "web")
+exec /usr/local/bin/etcdctl --peers ${ETCD_PEER}  exec-watch --recursive /services/$HAPROXY_BACKEND_SERVICE -- /update-haproxy.sh
+
--- update-haproxy.sh
+++ update-haproxy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ETCD_PEER="172.17.42.1:4001"
 HAPROXY_CONFIG="/etc/haproxy/haproxy.cfg"
 
 function update {
@@ -8,11 +9,13 @@ function update {
   HOST_IP=$(echo $ETCD_WATCH_VALUE | tr -d '"{} ' | cut -d, -f1 | cut -d: -f2)
   HOST_PORT=$(echo $ETCD_WATCH_VALUE | tr -d '"{} ' | cut -d, -f2 | cut -d: -f2)
 
-  HAPROXY_BACKEND_SERVER_MAX_CONNECTIONS=$(etcdctl get /config/HAPROXY_BACKEND_SERVER_MAX_CONNECTIONS || 32)
+  HAPROXY_BACKEND_SERVER_MAX_CONNECTIONS=$(etcdctl --peers ${ETCD_PEER} get /config/HAPROXY_BACKEND_SERVER_MAX_CONNECTIONS | grep -v "Error: 100: Key not found" || echo "32")
 
   if grep -q "server ${HOST_NAME}" $HAPROXY_CONFIG; then
+    echo "Updating host ${HOST_NAME}"
     sed -i -e "s/  server ${HOST_NAME}.*$/  server ${HOST_NAME} ${HOST_IP}:${HOST_PORT} maxconn ${HAPROXY_BACKEND_SERVER_MAX_CONNECTIONS} check/g" $HAPROXY_CONFIG
   else
+    echo "Adding host ${HOST_NAME}"
     echo "  server ${HOST_NAME} ${HOST_IP}:${HOST_PORT} maxconn ${HAPROXY_BACKEND_SERVER_MAX_CONNECTIONS} check" >> $HAPROXY_CONFIG
   fi
 
@@ -22,6 +25,7 @@ function remove {
 
   HOST_NAME=$(echo $ETCD_WATCH_KEY | cut -d/ -f4)
 
+  echo "Removing host ${HOST_NAME}"
   sed -i'' "/server ${HOST_NAME} /d" $HAPROXY_CONFIG
 
 }
@@ -38,5 +42,5 @@ case $ETCD_WATCH_ACTION in
     exit 1
 esac
 
-/usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
+exec /usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
 
