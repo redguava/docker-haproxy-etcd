cfb90bc944f201de4867c6d4d41778148ed20325
Matthew Jones <matt@geekle.id.au>
2014-04-05 23:43:20 +1100

    Check the config before starting haproxy

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-exec /usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
+/usr/local/sbin/haproxy -c -q -f /etc/haproxy/haproxy.cfg
+if [ $? -ne 0 ]; then
+        echo "Errors in configuration file, check with $prog check."
+        exit 1
+fi
+/usr/local/sbin/haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
+while [ $(pidof haproxy 1>/dev/null; echo $?) -eq 0 ]; do
+        sleep 3
+        echo -n "."
+done
